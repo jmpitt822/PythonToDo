157e0116a5d9b7332c6c9609199189e0c7a6a6bf
Jeremy Pitt <jeremypitt@Jeremys-MacBook-Pro.local>
2017-02-17 15:08:54 -0600

    created ToDoItem class and made completion status toggleable

--- ToDo.py
+++ ToDo.py
@@ -1,30 +1,39 @@
+class ToDoItem:
+    def __init__(self, text):
+        self.text = text
+        self.isDone = False
+
+    def __str__(self):
+        return self.text
+
 toDoList = []
 
 
-def printlist():
+def printList():
     counter = 1
     for x in toDoList:
-        if x[1] == False:
-            print(counter, ".", x[0], ": [ ]")
+        if x.isDone is False:
+            print(counter, ".", x.text, ": [ ]")
         else:
-            print(counter, ".", x[0], ": [X]")
+            print(counter, ".", x.text, ": [X]")
         counter += 1
 
 def createToDo():
-    newToDo = input("What do you need to do today?\n")
-    toDoList.append((newToDo, False))
+    newText = input("What do you need to do today?\n")
+    newItem = ToDoItem(newText)
+    toDoList.append(newItem)
 
 def updateToDo():
-    updateItem = int(input("Which item do you want to toggle?")) - 1
-    print(toDoList[updateItem])
-    # toDoList[updateToDo] = not toDoList[updateToDo]
+    updateItem = int(input("Which item do you want to toggle?\n")) - 1
+    toDoList[updateItem].isDone = not toDoList[updateItem].isDone
 
 
 while True:
-    printlist()
-    # print(toDoList)
+    printList()
     choice = input("What would you like to do? 1. Create a To Do Item | 2. Toggle To Do Item | 3. Cancel\n")
     if choice == "1":
         createToDo()
     elif choice == "2":
         updateToDo()
+    elif choice == "3":
+        exit()
